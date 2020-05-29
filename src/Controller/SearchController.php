f216ced5450fbfb77e146937b4820952ea7251b6
Hwa-Seon Choi <hwaseon.lecouey@gmail.com>
2020-05-29 16:21:22 +0200

    Add SF form component

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/search", name="search", methods={"GET"})
+     * @return string
+     */
+    public function search(Request $request)
+    {
+        $query = $request->query->get('q');
+        return new Response('200',200);
+    }
+}
