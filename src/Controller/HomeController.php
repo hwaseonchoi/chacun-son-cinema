3339d2ff6ba99a8875d62411939fa1bb7d47bbdf
Hwa-Seon Choi <hwaseon.lecouey@gmail.com>
2020-11-26 20:29:33 +0100

    Add two fonts and home page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\TmdbApiService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home", methods={"GET"})
+     */
+    public function home()
+    {
+        return $this->render('home.html.twig');
+    }
+}
